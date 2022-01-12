5eacc9ea77a7deff41f057dff49f8274b2cc634e
Rimantas <belauzas@yahoo.com>
2022-01-12 22:20:18 +0200

    File system

--- files.js
+++ files.js
@@ -2,14 +2,24 @@ const fs = require('fs/promises');
 
 (async () => {
 
-    Math.round();
-
     const drinksContent = await fs.readFile('./data/drinks.txt', 'utf-8');
-    console.log(drinksContent);
-    console.log(typeof drinksContent);
+    const drinksList = drinksContent.split('\r\n');
 
-    for (const letter of drinksContent) {
-        console.log(letter);
+    // Pasirinkimas 1: [gerimo-pavadinimas] kurio kaina yra [kaina] EUR.
+    // Pasirinkimas 2: [gerimo-pavadinimas] kurio kaina yra [kaina] EUR.
+    // Pasirinkimas 3: [gerimo-pavadinimas] kurio kaina yra [kaina] EUR.
+
+    let i = 0;
+    for (const drink of drinksList) {
+        // const drinkDetails = drink.split(' ');
+        // const name = drinkDetails[0];
+        // const price = drinkDetails[1];
+        // const count = drinkDetails[2];
+
+        const drinkDetails = drink.split(' ');
+        const [name, price, count] = drinkDetails;
+
+        console.log(`Pasirinkimas ${++i}: ${name} kurio kaina yra ${price} EUR buvo parduota ${count} kiekis ir bendra apyvarta yra [apyvarta] EUR.`);
     }
 
 })();
