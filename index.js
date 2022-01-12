1f1c5ba24576d7982edcb11896326fdbc80e5a60
Rimantas <belauzas@yahoo.com>
2022-01-12 18:54:20 +0200

    Loops

--- index.js
+++ index.js
@@ -30,11 +30,13 @@ const s2 = 'Chuck Norris';
 const s3 = 'Wonder WomaN';
 const s4 = 'Algimantas cekuolis';
 
-function inicialai(pavadinimas) {
-    const word = pavadinimas.split(' ');
-    const vardas = word[0][0].toUpperCase();
-    const pavarde = word[1][0].toUpperCase();
-    console.log(`Legendines asmenybes ${word[0]}${word[1]}trumpinys yra ${vardas}.${pavarde}.`);
+function inicialai(text) {
+    const words = text.split(' ');
+    const name = words[0];
+    const nameFirstLetter = name[0].toUpperCase();
+    const surname = words[1];
+    const surnameFirstLetter = surname[0].toUpperCase();
+    console.log(`Legendines asmenybes ${name} ${surname} trumpinys yra ${nameFirstLetter}.${surnameFirstLetter}.`);
 }
 
 inicialai(s1);
@@ -49,4 +51,53 @@ const people = [
     { name: 'Maryte', surname: 'Marytaite', age: 87 },
 ];
 
-console.log(`${people[0].name} ${people[0].surname} yra ${people[0].age} metu amziaus`);
+function introduce(person) {
+    console.log(`${person.name} ${person.surname} yra ${person.age} metu amziaus`);
+}
+
+console.log('-------- for');
+
+const forSize = people.length;
+for (let i = 0; i < forSize; i++) {
+    const person = people[i];
+    introduce(person);
+}
+
+console.log('-------- while');
+
+let iw = 0;
+const whileSize = people.length;
+while (iw < whileSize) {
+    const person = people[iw];
+    introduce(person);
+    iw++;
+}
+
+console.log('-------- do-while');
+
+let idw = 0;
+const doWhileSize = people.length;
+do {
+    const person = people[idw];
+    introduce(person);
+    idw++;
+} while (idw < doWhileSize);
+
+console.log('-------- for-of');
+
+for (const person of people) {
+    introduce(person);
+}
+
+console.log('-------- for-in');
+
+for (const i in people) {
+    const person = people[i]
+    introduce(person);
+}
+
+console.log('-------- foreach');
+
+people.forEach((person) => {
+    introduce(person);
+})
