78cfc4c1fabded38f92f2c9018361d6763915ebb
Rimantas <belauzas@yahoo.com>
2022-01-13 19:01:09 +0200

    Countries task

---
+++ country.js
@@ -0,0 +1,1 @@
+// [Parent-name] [Parent-surname] has [count] children and they names are: [Child-name], [Child-name], [Child-name].
--- files.js
+++ files.js
@@ -11,15 +11,14 @@ const fs = require('fs/promises');
 
     let i = 0;
     for (const drink of drinksList) {
-        // const drinkDetails = drink.split(' ');
-        // const name = drinkDetails[0];
-        // const price = drinkDetails[1];
-        // const count = drinkDetails[2];
-
         const drinkDetails = drink.split(' ');
         const [name, price, count] = drinkDetails;
+        const balance = +price * +count;
+        const precision = 2;
+        const b1 = balance.toFixed(precision);
+        const b2 = Math.round(balance * (10 ** precision)) / (10 ** precision);
 
-        console.log(`Pasirinkimas ${++i}: ${name} kurio kaina yra ${price} EUR buvo parduota ${count} kiekis ir bendra apyvarta yra [apyvarta] EUR.`);
+        console.log(`Pasirinkimas ${++i}: ${name} kurio kaina yra ${price} EUR buvo parduota ${count} kiekis ir bendra apyvarta yra ${b1} EUR.`);
     }
 
 })();
