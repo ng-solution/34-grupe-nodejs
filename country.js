d66700f20bbc500f9ff18df56c938314ead3922a
Rimantas <belauzas@yahoo.com>
2022-01-13 19:19:45 +0200

    Parsed JSON

--- country.js
+++ country.js
@@ -1,1 +1,21 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+// console.log(path);
+
+console.log(__dirname);
+console.log(__filename);
+
 // [Parent-name] [Parent-surname] has [count] children and they names are: [Child-name], [Child-name], [Child-name].
+
+(async () => {
+
+    const fullPath = path.join(__dirname, './data/country.json');
+    const countryContent = await fs.readFile(fullPath, 'utf-8');
+
+    console.log(countryContent);
+
+    const personInfo = JSON.parse(countryContent);
+    console.log(personInfo);
+
+})();
